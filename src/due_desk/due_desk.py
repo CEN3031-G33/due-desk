b0215fd02d26a91c2e16c4b50e9ddaf444bd5969
Chase Ruskin <c.ruskin@ufl.edu>
2022-03-05 19:21:34 -0500

    adds basic example window

--- src/due_desk/due_desk.py
+++ src/due_desk/due_desk.py
@@ -8,9 +8,31 @@
 
 import unittest
 
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+def window():
+    '''
+    Example code to open a basic window to demonstrate pyqt5 is properly 
+    installed.
+    '''
+    app = QApplication(sys.argv)
+    w = QWidget()
+    b = QLabel(w)
+    b.setText("Welcome to your DueDesk!")
+    w.setGeometry(100,100,200,50)
+    b.move(50,20)
+    w.setWindowTitle("Due Desk")
+    w.show()
+    sys.exit(app.exec_())
+
+
 def due_desk():
     print("Welcome to your DueDesk!")
-
+    window()
 
 def main():
     due_desk()
