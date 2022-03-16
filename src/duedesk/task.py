200e22d55f064ba517e1f3a043a456a5fe0dd0d2
Chase Ruskin <c.ruskin@ufl.edu>
2022-03-15 21:56:12 -0400

    adds basic task class

--- src/duedesk/task.py
+++ src/duedesk/task.py
@@ -7,15 +7,57 @@
 # ------------------------------------------------------------------------------
 import unittest
 
+
 class Task:
-    def __init__(self):
-        self.subject = ''
-        self.deadline = ''
+    def __init__(self, subject, deadline) -> None:
+        '''Creates a new `Task` object.'''
+        self._subject = subject
+        self._deadline = deadline
         pass
 
+
+    def get_subject(self) -> str:
+        return self._subject
+
+
+    def get_deadline(self) -> str:
+        return self._deadline
+
+
+    def set_subject(self, s) -> None:
+        self._subject = s
+
+
+    def set_deadline(self, d) -> None:
+        self._deadline = d
+
+
+    def __repr__(self) -> str:
+        '''Represent `Task` as a str.'''
+        repr = '\ntopic: '+self._subject+'\ndue: '+self._deadline+'\n\n'
+        return repr
     pass
 
 
 class TestTask(unittest.TestCase):
-    def new(self):
-        self.assertEqual(1 + 1, 2)
+    def test_new(self):
+        t = Task('write task class', '2022-01-01')
+        self.assertEqual(t._subject, 'write task class')
+        self.assertEqual(t._deadline, '2022-01-01')
+        # print(t.__repr__())
+        pass
+
+
+    def test_modifiers_accessors(self):
+        t = Task('nothing', 'never')
+
+        self.assertEqual(t.get_subject(), 'nothing')
+        self.assertEqual(t.get_deadline(), 'never')
+
+        t.set_subject('read a book')
+        self.assertEqual(t.get_subject(), 'read a book')
+
+        t.set_deadline('2022-03-15')
+        self.assertEqual(t.get_deadline(), '2022-03-15')
+
+        pass
