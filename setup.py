1291c6bd3b3caa2d3dce164860394c019cd8107f
Chase Ruskin <c.ruskin@ufl.edu>
2022-03-16 20:43:34 -0400

    ci: adds pyqt5 ext. dep. and fixes naming issues

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     entire_description = fh.read()
 
-exec(open('src/due_desk/__version__.py').read())
+exec(open('src/duedesk/__version__.py').read())
 setuptools.setup(
     name="DueDesk",
     version=__version__,
@@ -26,10 +26,10 @@ setuptools.setup(
     classifiers=[
         "",
     ],
-    install_requires=[],
+    install_requires=['PyQt5'],
     entry_points='''
             [console_scripts]
-            duedesk=due_desk.due_desk:main
+            duedesk=duedesk.duedesk:main
         ''',
     package_dir={"": "src"},
     package_data={"": []},
--- src/duedesk/duedesk.py
+++ src/duedesk/duedesk.py
@@ -1,31 +1,26 @@
 # ------------------------------------------------------------------------------
-# Project: DueDesk
-# Module: due_desk
-#
-# Abstract: 
+# Project  : DueDesk
+# Module   : duedesk
+# Abstract : 
 #   The main script called to begin the DueDesk application.
 # ------------------------------------------------------------------------------
-
 import unittest
-
 from .window import *
-
-
 
 def due_desk():
     window()
+
 
 def main():
     due_desk()
 
 
-#--- entry-point ---
 if __name__ == "__main__":
     main()
 
 
-#--- unit-tests ----
 class TestDueDesk(unittest.TestCase):
-
     def test_sample_unit(self):
         self.assertEqual(1 + 1, 2)
+        pass
+    pass
