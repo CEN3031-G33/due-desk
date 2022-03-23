8a08c3fa12a355d3eb3ed6b60a4e5cfc1015bc11
Josh <david10.prentice@gmail.com>
2022-03-22 21:02:19 -0400

    initial work on resourcepool

--- src/duedesk/resourcepool.py
+++ src/duedesk/resourcepool.py
@@ -1,16 +1,17 @@
 # ------------------------------------------------------------------------------
 # Project  : DueDesk
-# Module   : task
+# Module   : Resourcepool
 # Abstract : 
-#   A resource list is a manager of multiple resources. The resource list
-#   allows interaction with a variety of resources
+#   A resource list is a manager of multiple resources. It will be used to
+#   represent the pool of items the user can add to their desk and the items
+#   currently on the desk.
 # ------------------------------------------------------------------------------
 
 import unittest
 from typing import List
 from .resource import Resource
 
-class Resourcelist:
+class Resourcepool:
     def __init__(self, inner: List[Resource]):
         '''Creates a 'Resourcelist' object'''
         self._inner = inner
@@ -24,7 +25,7 @@ class Resourcelist:
 
 class TestResourcelist(unittest.TestCase):
     def test_new_and_get_by_index(self):
-        rl = Resourcelist([
+        rl = Resourcepool([
             Resource("file", (1, 2), True, False, 3),
             Resource("./README.md", (1, 2), True, False, 3),
             Resource("./docs/images/superdesk.png", (1, 2), True, False, 3)
